98b9ff0aa227be75568ea287b433d48571005154
hitachiuhg <hardeep.hmis@gmail.com>
2019-10-14 15:33:32 +0530

    Files to commit

---
+++ ami_backup.py
@@ -0,0 +1,51 @@
+# Automated AMI Backups
+#
+# "retention_days" is environment variable which will be used as a retention policy number in days. If there is no
+# environment variable with that name, it will use a 7 days default value for each AMI.
+#
+# After creating the AMI it creates a "DeleteOn" tag on the AMI indicating when
+# it will be deleted using the Retention value and another Lambda function
+
+from __future__ import print_function
+import boto3
+import collections
+import datetime
+import sys
+import pprint
+import os
+import json
+
+ec = boto3.client('ec2')
+ec2_instance_id = os.environ['instance_id']
+label_id = os.environ['label_id']
+no_reboot = os.environ['reboot'] == '0'
+block_device_mappings = json.loads(str(os.environ['block_device_mappings']))
+
+def lambda_handler(event, context):
+    try:
+        retention_days = int(os.environ['retention'])
+    except ValueError:
+        retention_days = 7
+    create_time = datetime.datetime.now()
+    create_fmt = create_time.strftime('%Y-%m-%d')
+    AMIid = ec.create_image(InstanceId=ec2_instance_id,
+                            Name=label_id + "-" + ec2_instance_id + "-" + create_fmt,
+                            Description=label_id + "-" + ec2_instance_id + "-" + create_fmt,
+                            NoReboot=no_reboot, DryRun=False,
+                            BlockDeviceMappings=block_device_mappings)
+
+    print("Retaining AMI %s of instance %s for %d days" % (
+        AMIid['ImageId'],
+        ec2_instance_id,
+        retention_days,
+    ))
+
+    delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
+    delete_fmt = delete_date.strftime('%m-%d-%Y')
+
+    ec.create_tags(
+        Resources=[AMIid['ImageId']],
+        Tags=[
+            {'Key': 'DeleteOn', 'Value': delete_fmt},
+        ]
+    )
---
+++ variables.tf
@@ -0,0 +1,56 @@
+# See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
+# for how to write schedule expressions
+variable "backup_schedule" {
+  default     = "cron(00 19 * * ? *)"
+  description = "The scheduling expression. (e.g. cron(0 20 * * ? *) or rate(5 minutes)"
+}
+
+variable "cleanup_schedule" {
+  default     = "cron(05 19 * * ? *)"
+  description = "The scheduling expression. (e.g. cron(0 20 * * ? *) or rate(5 minutes)"
+}
+
+variable "ami_owner" {
+  default     = ""
+  description = "AWS Account ID which is used as a filter for AMI list (e.g. `123456789012`)"
+}
+
+variable "region" {
+  default     = ""
+  description = "AWS Region where module should operate (e.g. `us-east-1`)"
+}
+
+variable "retention_days" {
+  default     = "7"
+  description = "Is the number of days you want to keep the backups for (e.g. `7`)"
+}
+
+variable "instance_id" {
+  description = "AWS Instance ID which is used for creating the AMI image (e.g. `id-123456789012`)"
+}
+
+variable "block_device_mappings" {
+  description = "List of block device mappings to be included/excluded from created AMIs. With default value of [], AMIs will include all attached EBS volumes "
+  type        = "list"
+  default     = []
+}
+
+variable "name" {
+  default     = ""
+  description = "Name  (e.g. `bastion` or `db`)"
+}
+
+variable "namespace" {
+  default     = ""
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  default     = ""
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "reboot" {
+  default     = "false"
+  description = "Reboot the machine as part of the snapshot process"
+}
